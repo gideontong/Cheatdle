5e30b142c974d91374682bc1f55f3fae9b2f3d81
Gideon Tong <gideon@gideontong.com>
2022-01-30 17:58:49 -0800

    Code that kind of works

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Cheatdle
+
+*You aren't seriously trying to cheat in a game where it's only bragging rights with your friends, right?*
+
 Unbeatable Guesser in Wordle and Wordle Clones
+
+## FAQ
+
+* You need to extract the word list yourself. Some have been provided, but they may not be updated.
+* No, I won't help with word list extraction.
+* Yes, dordle support is coming.
+* Yes, the codebase is messy.
---
+++ app.py
@@ -0,0 +1,67 @@
+from os import listdir
+from random import choice
+
+DATA_FOLDER = 'data'
+
+def return_correctness(word: str) -> tuple:
+    ans = [None] * 5
+    letters = list()
+    not_in = set()
+    for i, c in enumerate(word):
+        prompt = input(f'was {c} correct (yes/misplaced/no)? ')
+        if prompt == 'yes':
+            ans[i] = c
+        elif prompt == 'misplaced':
+            letters.append(c)
+        elif prompt == 'no':
+            not_in.add(c)
+    
+    return ans, letters, not_in
+
+def remove_possible(ans: list, letters: list, not_in: set, wordlist: list) -> list:
+    for i, c in enumerate(ans):
+        if c:
+            for word in wordlist.copy():
+                if word[i] != c:
+                    wordlist.remove(word)
+    
+    if len(letters) > 0:
+        for word in wordlist:
+            exist = False
+            for letter in letters:
+                if letter in word:
+                    exist = True
+            
+            if not exist:
+                wordlist.remove(word)
+
+    for c in not_in:
+        for word in wordlist.copy():
+            if c in word:
+                wordlist.remove(word)
+    return wordlist
+
+wordlists = set([file.split('.')[0] for file in listdir(DATA_FOLDER)])
+print('allowed wordlists:', wordlists)
+
+wordlist = input('name of wordlist to use? ')
+while wordlist not in wordlists:
+    wordlist = input('invalid, enter a valid name? ')
+
+with open(f'{DATA_FOLDER}/{wordlist}.txt') as fp:
+    words = fp.readlines()
+
+words = [word.strip() for word in words]
+print('added', len(words), 'words to analysis')
+
+# TODO: analyze which guess is the most likely to remove the most words
+# for now, just pick a random word
+
+while len(words) > 1:
+    guess = choice(words)
+    print(len(words), 'left, next guess is:', guess)
+
+    word, letters, not_in = return_correctness(guess)
+    words = remove_possible(word, letters, not_in, words)
+
+    print(word, letters, words)
