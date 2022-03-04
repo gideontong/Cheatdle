9960bd47d85412f37f15c9724433cbfb5441d745
Gideon Tong <gideon@gideontong.com>
2022-03-03 20:19:23 -0800

    New code

--- best.py
+++ best.py
@@ -2,17 +2,29 @@ from itertools import combinations
 from tqdm import tqdm
 from json import dump
 from math import comb
+from time import sleep
 
 with open('data/wordle.txt') as fp:
     words = [x.strip() for x in fp.readlines()]
 
-out = list()
-with tqdm(total=comb(len(words), 3)) as bar:
-    for word in combinations(words, 3):
-        if len(set(''.join(list(word)))) == 15:
-            print(word)
-            out.append(word)
-        bar.update(1)
+with open('best.txt', 'w') as fp:
+    with tqdm(total=comb(len(words), 3)) as bar:
+        for i, w1 in enumerate(words):
+            if len(set(w1)) < 5:
+                bar.update((len(words) - i) ** 2)
+                continue
+            for j, w2 in enumerate(words[i+1:]):
+                if len(set(w2)) < 5 or len(set(w1 + w2)) < 10:
+                    bar.update(len(words) - j)
+                    continue
+                for k, w3 in enumerate(words[j+1:]):
+                    if len(set(w3)) < 5 or len(set(w1 + w2 + w3)) < 15:
+                        bar.update(1)
+                        continue
+                    nex = f'{w1} {w2} {w3}\n'
+                    if all([x in nex for x in 'aeiounptsh']):
 
-with open('data/triple.json') as fp:
-    dump(out, fp)
+                        print(w1, w2, w3)
+                    
+                    fp.write(nex)
+                    bar.update(1)
